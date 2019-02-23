64a1ed2ea73af25d55f8a79a62860020dc0208cf
Ignacio Gutierrez <igutierrez@medallia.com>
2019-02-23 16:19:32 -0300

    validation

--- src/Tests.java
+++ src/Tests.java
@@ -5,8 +5,42 @@ import java.util.List;
 public class Tests {
 
 	public static void main(String[] args) throws IOException {
+		validateValidation1();
+		validateValidation2();
 		test1();
 		test2();
+	}
+
+	public static void validateValidation1() throws IOException {
+		RidesProblemInput input = Problem.read("./inputs/a_example.in");
+		List<List<Integer>> outputList = Arrays.asList(
+				Arrays.asList(0),
+				Arrays.asList(2, 1, 0)
+		);
+		RidesProblemOutput output = new RidesProblemOutput(outputList);
+
+		try {
+			RidesValidator.validateSolution(input, output);
+			throw new RuntimeException("expected duplicated rides exception");
+		} catch(IllegalArgumentException e) {
+
+		}
+	}
+
+	public static void validateValidation2() throws IOException {
+		RidesProblemInput input = Problem.read("./inputs/a_example.in");
+		List<List<Integer>> outputList = Arrays.asList(
+				Arrays.asList(0),
+				Arrays.asList(3, 1)
+		);
+		RidesProblemOutput output = new RidesProblemOutput(outputList);
+
+		try {
+			RidesValidator.validateSolution(input, output);
+			throw new RuntimeException("expected invalid ride number exception");
+		} catch(IllegalArgumentException e) {
+
+		}
 	}
 
 	public static void test1() throws IOException {
