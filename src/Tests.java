763cb893b70bd837c234d46e0730537b881b5d7c
santialvarezcolombo <santialvarezcolombo@gmail.com>
2019-02-28 16:03:56 -0300

    Another test

--- src/Tests.java
+++ src/Tests.java
@@ -9,6 +9,7 @@ public class Tests {
     public static void main(String[] args) throws IOException {
         validateValidation1();
         validateValidation2();
+        validateValidation3();
         test1();
     }
 
@@ -45,6 +46,25 @@ public class Tests {
         try {
             Validator.validateSolution(input, output);
             throw new RuntimeException("expected a vertical photo in the same slide as an horizontal one");
+        } catch (IllegalStateException e) {
+
+        }
+    }
+
+    public static void validateValidation3() throws IOException {
+        Input input = Main.read("a_example");
+        List<Slide> slides = Arrays.asList(
+                new Slide(Arrays.asList(new Photo(0, Photo.Orientation.H, new HashSet<>(Arrays.asList("cat", "beach", " sun"))))),
+                new Slide(Arrays.asList(new Photo(3, Photo.Orientation.H, new HashSet<>(Arrays.asList("garden", "cat"))))),
+                new Slide(Arrays.asList(new Photo(4, Photo.Orientation.H, new HashSet<>(Arrays.asList("garden", "selfie"))),
+                        new Photo(2, Photo.Orientation.V, new HashSet<>(Arrays.asList("selfie", "smile")))))
+        );
+
+        Output output = new Output(slides.size(), slides);
+
+        try {
+            Validator.validateSolution(input, output);
+            throw new RuntimeException("expected a photo with id bigger than the number of photos in the problem");
         } catch (IllegalStateException e) {
 
         }
