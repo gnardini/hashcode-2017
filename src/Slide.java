9b30e1d0fd21daae7b5b73a5b28dcf5976bbcd22
Gustavo Juan Cairo <gcairo@medallia.com>
2019-02-28 16:26:18 -0300

    Adds comparison to slides

--- src/Slide.java
+++ src/Slide.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,4 +19,33 @@ public class Slide {
         }
         this.tags = tags;
     }
+
+    public SlideScore tagComparison(Slide otherSlide) {
+		Set<String> union = new HashSet<>(this.tags);
+		union.addAll(new HashSet<>(otherSlide.tags));
+		Set<String> onlyInCurrentPhoto = new HashSet<>(this.tags);
+		Set<String> onlyInNextPhoto = otherSlide.tags;
+		onlyInCurrentPhoto.removeAll(onlyInNextPhoto);
+    	return new SlideScore(onlyInCurrentPhoto.size(), onlyInNextPhoto.size(), union.size());
+	}
+
+	public int transitionScoreTo(Slide otherSlide) {
+    	return tagComparison(otherSlide).min();
+	}
+
+	class SlideScore {
+		public int tagsOnlyInCurrentPhoto;
+		public int tagsOnlyInNextPhoto;
+		public int tagsInBothPhotos;
+
+		public SlideScore(int tagsOnlyInCurrentPhoto, int tagsOnlyInNextPhoto, int tagsInBothPhotos) {
+			this.tagsOnlyInCurrentPhoto = tagsOnlyInCurrentPhoto;
+			this.tagsOnlyInNextPhoto = tagsOnlyInNextPhoto;
+			this.tagsInBothPhotos = tagsInBothPhotos;
+		}
+
+		public int min() {
+			return Collections.min(Arrays.asList(tagsOnlyInCurrentPhoto, tagsInBothPhotos, tagsOnlyInNextPhoto));
+		}
+	}
 }
