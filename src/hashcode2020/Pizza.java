b47ba41a1d8888a402ad2399659e3af281a83d8e
Ignacio Gutierrez <igutierrez@medallia.com>
2020-01-30 19:19:08 -0300

    all

---
+++ src/hashcode2020/Pizza.java
@@ -0,0 +1,13 @@
+package hashcode2020;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Pizza {
+
+	static List<String> files = Arrays.asList("a_example", "b_lovely_landscapes", "c_memorable_moments", "d_pet_pictures", "e_shiny_selfies");
+
+	public static void main(String[] args) {
+
+	}
+}
