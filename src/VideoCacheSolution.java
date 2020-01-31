5ce3890f7089ec5ac0e968065b58509a032c6fbc
gnardini <gonzalo.nardini.dev@gmail.com>
2020-01-30 22:32:35 -0300

    more stuff

--- src/VideoCacheSolution.java
+++ src/VideoCacheSolution.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 public class VideoCacheSolution implements Problem {
 
+    static int videoAverage;
     static int[][] endpointVideoRequests;
     static Map<Integer, List<Input.Endpoint>> cachesToEndpoints;
 
     static class VideoCacheTimeSaved implements Comparable<VideoCacheTimeSaved> {
         final int videoId;
         final int cacheId;
-        final int timeSaved;
+        final double timeSaved;
 
-        public VideoCacheTimeSaved(int videoId, int cacheId, int timeSaved) {
+        public VideoCacheTimeSaved(int videoId, int cacheId, double timeSaved) {
             this.videoId = videoId;
             this.cacheId = cacheId;
             this.timeSaved = timeSaved;
@@ -18,12 +19,17 @@ public class VideoCacheSolution implements Problem {
 
         @Override
         public int compareTo(VideoCacheTimeSaved o) {
-            return Integer.compare(timeSaved, o.timeSaved);
+            return Double.compare(o.timeSaved, timeSaved);
         }
     }
 
     @Override
     public Output solve(Input input) {
+        int totalVideoSizes = 0;
+        for (int videoSize : input.videoSizes) {
+            totalVideoSizes += videoSize;
+        }
+        videoAverage = totalVideoSizes / input.V;
         endpointVideoRequests = new int[input.E][input.V];
         for (Input.Request r : input.Rs) {
             endpointVideoRequests[r.endpointId][r.videoId] += r.R;
@@ -31,36 +37,67 @@ public class VideoCacheSolution implements Problem {
         cachesToEndpoints = new HashMap<>();
         for (int i = 0; i < input.Es.length; i++) {
             for (Integer cacheId : input.Es[i].caches.keySet()) {
-                cachesToEndpoints.getOrDefault(cacheId, new ArrayList<>()).add(input.Es[i]);
+                List<Input.Endpoint> endpoints = cachesToEndpoints.getOrDefault(cacheId, new ArrayList<>());
+                endpoints.add(input.Es[i]);
+                cachesToEndpoints.put(cacheId, endpoints);
             }
         }
 
         PriorityQueue<VideoCacheTimeSaved> orderedStuff = new PriorityQueue<>();
 
-        int[][] m = new int[input.V][input.C];
+
+        double[][] m = new double[input.V][input.C];
         for (int i = 0; i < input.V; i++) {
             for (int j = 0; j < input.C; j++) {
-                m[i][j] = timeSavedVideoInCache(i, j);
+                m[i][j] = timeSavedVideoInCache(input, i, j);
                 orderedStuff.add(new VideoCacheTimeSaved(i, j, m[i][j]));
+            }
+            if ( i % 100 == 0) {
+                System.out.println(i);
             }
         }
 
         Map<Integer, Set<Integer>> output = new HashMap<>();
 
+        int[] totalCachesSizes = new int[input.V];
+        for (int i = 0; i < input.V; i++) {
+            int totalInLine = 0;
+            for (int j = 0; j < input.C; j++) {
+                totalInLine += m[i][j];
+            }
+            totalCachesSizes[i] = totalInLine;
+        }
+
         int[] cacheSizes = new int[input.C];
         while (!orderedStuff.isEmpty()) {
+//            System.out.println(orderedStuff.size());
             VideoCacheTimeSaved next = orderedStuff.poll();
+//            double currentValue = m[next.videoId][next.cacheId];
+//            if (next.timeSaved != currentValue) {
+//                orderedStuff.add(new VideoCacheTimeSaved(next.videoId, next.cacheId, currentValue));
+//                continue;
+//            }
             if (cacheSizes[next.cacheId] + input.videoSizes[next.videoId] < input.X) {
-                cacheSizes[next.cacheId] += input.videoSizes[next.videoId];
-                output.getOrDefault(next.cacheId, new HashSet<>()).add(next.videoId);
+                double multiplier = videoAverage*videoAverage > input.videoSizes[next.videoId]*input.videoSizes[next.videoId] ? 2 : .8;
+                if ((next.timeSaved / totalCachesSizes[next.videoId]) * multiplier > Math.random()) {
+                    cacheSizes[next.cacheId] += input.videoSizes[next.videoId];
+                    Set<Integer> videos = output.getOrDefault(next.cacheId, new HashSet<>());
+                    videos.add(next.videoId);
+                    output.put(next.cacheId, videos);
+//                    for (Input.Endpoint endpoint : cachesToEndpoints.get(next.cacheId)) {
+//                        for (Integer cache : endpoint.caches.keySet()) {
+//                            m[next.videoId][cache] = timeSavedVideoInCache(input, next.videoId, cache);
+//                        }
+//                    }
+                }
             }
         }
 
         return new Output(output);
     }
 
-    private int timeSavedVideoInCache(int videoId, int cacheId) {
-        int timeSaved = 0;
+    private double timeSavedVideoInCache(Input input, int videoId, int cacheId) {
+        double timeSaved = 0;
         List<Input.Endpoint> endpoints = cachesToEndpoints.get(cacheId);
         if (endpoints == null || endpoints.isEmpty()) {
             return 0;
@@ -68,6 +105,7 @@ public class VideoCacheSolution implements Problem {
         for (Input.Endpoint endpoint : endpoints) {
             timeSaved += endpointVideoRequests[endpoint.id][videoId] * (endpoint.latency - endpoint.caches.get(cacheId));
         }
-        return timeSaved;
+        int videoSize = input.videoSizes[videoId];
+        return timeSaved / Math.log(videoSize);
     }
 }
